f5097d23a7823a9cad211fabdc240123d32a0175
MicaelSoares7 <micaelsoares0706@gmail.com>
2024-10-16 10:51:52 -0300

    update

--- lista 1/prg016.py
+++ lista 1/prg016.py
@@ -1,5 +1,5 @@
 carro = float(input("Preco do carro: "))
 
-preco = carro * (45+28) / 100 + carro
+preco = (carro * ((45+28) / 100)) + carro
 
-print("Preco do carro: R$ %5.2f" % preco)
+print(f"Preco do carro: R$ {preco:.2f}")
