cae0259f479160c64dc10828db10fe01f9df2b6b
MicaelSoares7 <micaelsoares0706@gmail.com>
2024-10-26 15:56:30 -0300

    update

---
+++ exercicios/parabens.py
@@ -0,0 +1,11 @@
+n1 = float(input("Nota 1: "))
+n2 = float(input("Nota 2: "))
+
+media = (n1 + n2) / 2
+
+print(f"Media: {media}")
+
+if (media >= 7):
+    print("Parabéns!")
+else:
+    print("Estude um pouco mais.")
