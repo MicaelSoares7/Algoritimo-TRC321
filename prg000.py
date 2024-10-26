905beb061be220d50e5631b07f13a46546d95443
MicaelSoares7 <micaelsoares0706@gmail.com>
2024-10-26 15:55:57 -0300

    update

--- prg000.py
+++
@@ -1,8 +0,0 @@
-#Faça um programa que peça 2 números e mostre o resultado da soma deles.
-
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite outro número:"))
-
-soma = n1+n2
-
-print("O resultado da soma é:", soma)
--- prg001.py
+++
@@ -1,7 +0,0 @@
-#Faça um programa que peça um número e mostre no resultado o número ao quadrado
-
-num = int(input("Digite um número: "))
-
-quadrado = num ** 2 # sinal de elevado -> **
-
-print("O numero", num, " ao quadrado é", quadrado)
